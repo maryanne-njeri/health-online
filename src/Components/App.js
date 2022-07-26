471ee49f1f80a88f825cfdcb2b4c1add5c7e74f7
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-07-26 12:55:44 +0300

    adds Tailwind CSS and created components

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import About from "./About";
+import Home from "./Home";
+import Condition from "./Condition";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />    
+      <About />    
+      <Condition />    
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Condition.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Condition() {
+    return (
+        <div>Condition</div>
+    )
+}
+
+export default Condition;
